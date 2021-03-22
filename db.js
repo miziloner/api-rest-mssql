6a9f3d34233ceee3dc749410a90cf9a6c1dbb469
miziloner <mizilonerdepumas@gmail.com>
2021-03-22 16:07:33 -0600

    Cannot get num variable

--- db.js
+++ db.js
@@ -2,7 +2,13 @@ const sql = require("mssql");
 
 
 const poolPromise = new sql.ConnectionPool(getDBconfig()).connect().then(pool => {
-    console.log('Connected to MSSQL')
+    console.log('Connected to Fuel')
+    return pool
+})
+    .catch(err => console.log('Database Connection Failed! Bad Config: ', err))
+
+const poolPromise_everis = new sql.ConnectionPool(getDBconfigEveris()).connect().then(pool => {
+    console.log('Connected to Everis')
     return pool
 })
     .catch(err => console.log('Database Connection Failed! Bad Config: ', err))
@@ -17,6 +23,19 @@ function getDBconfig() {
         port: 8433
     }
 }
+
+
+function getDBconfigEveris() {
+    return {
+        user: 'dna-admin',
+        password: '3^WCD9Zxj^bN',
+        server: 'dna-sql-everis.database.windows.net',
+        database: 'dna_fuelmc_lab',
+        port: 1433
+    }
+}
+
+
 module.exports = {
-    sql, poolPromise
+    sql, poolPromise, poolPromise_everis
 }
--- routes.js
+++ routes.js
@@ -1,14 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const { poolPromise } = require("./db")
+const { poolPromise, poolPromise_everis } = require("./db")
 
-router.get("/", async (req, res) => {
+/*router.get("/", async (req, res) => {
     try {
         const pool = await poolPromise
-        const result = await pool.request()
-            //.input
-            .query("select * from dbo.FP_Estacion")
-        //console.log(recordset)
+        const result = await pool.request().query("select * from dbo.FP_Estacion")
         res.json(result.recordset[0])
 
     } catch (err) {
@@ -18,7 +15,46 @@ router.get("/", async (req, res) => {
     }
     /*res.send("Hola mundo")
     console.log("Hola mundo")*/
+//});*/
 
+router.post('/api/getid', async (req, res) => {
+    const usn = req.body.usn_id_usuario
+    const num = await getId(usn, res)
+
+    console.log("print num", num)
+
+    if (num != undefined) {
+        getCompetitors(num)
+    } else {
+        res.end("Response not valid ")
+    }
 });
+
+async function getId(usn, res) {
+    try {
+        const pool = await poolPromise
+        const result = pool.request().query("select USN_EST_CODIGO ,USN_ID_USUARIO ,USN_MCL_CODIGO, est_numero_permiso from FP_UsuariosNegocios inner join FP_Estacion fe on usn_est_codigo= fe.EST_CODIGO where USN_ID_USUARIO = '" + usn + "'", function (err, recordset) {
+            if (err) console.log(err)
+            const est_numero_permiso = recordset.recordset[0].est_numero_permiso;
+            console.log("el numero de permiso es ", est_numero_permiso)
+            return est_numero_permiso;
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+async function getCompetitors(x) {
+    try {
+        const pool2 = poolPromise_everis
+        const result2 = pool2.request().query("select * from dbo.Competitors where est_numero_permiso ='" + x + "'", function (err, recordset) {
+            if (err) console.log(err)
+            console.log(recordset)
+            return recordset
+        })
+    } catch {
+        console.log(err)
+    }
+}
 
 module.exports = router
