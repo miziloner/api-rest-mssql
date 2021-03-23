f8cc680a3370a4cf490c0c899a8a6db89492dee7
miziloner <mizilonerdepumas@gmail.com>
2021-03-23 11:36:40 -0600

    API gets id_cre and competitors from endpoint localhost:5000/api/getid

--- routes.js
+++ routes.js
@@ -20,74 +20,44 @@ const { poolPromise, poolPromise_everis, sql } = require("./db")
 
 
 router.post('/api/getid', async (req, res) => {
-    const usn = req.body.usn_id_usuario
+    const usn = req.body.usn_id_usuario;
     // let numero = await getId(usn)
-    const getid = () => {
-        getId(usn).then(result => {
-            console.log("..", result);
+    const getid = async () => {
+        queryNumPermiso(usn).then(result => {
+            console.log("id cre: ", result);
+            getCompetitors(result).then(lista => {
+                console.log("lista", lista);
+                res.json(lista);
+            })
         });
 
     };
-    getCompetitors(getid());
-    //getid();
+
+    getid();
 
 
 
-
-    /*getId(usn).then(b => {
-        console.log("..", b)
-    })*/
-
-    /*  console.log("print num", num)
-  
-      if (num != undefined) {
-          getCompetitors(num).then(x => {
-              console.log(x)
-          })
-      } else {
-          res.end("Response not valid ")
-      }*/
 });
 
-/*async function getId(usn, res) {
-    try {
-        const pool = await poolPromise
-        const result = pool.request().query("select USN_EST_CODIGO ,USN_ID_USUARIO ,USN_MCL_CODIGO, est_numero_permiso from FP_UsuariosNegocios inner join FP_Estacion fe on usn_est_codigo= fe.EST_CODIGO where USN_ID_USUARIO = '" + usn + "'", function (err, recordset) {
-            if (err) console.log(err)
-            const est_numero_permiso = recordset.recordset[0].est_numero_permiso;
-            console.log("el numero de permiso es ", est_numero_permiso)
-            return est_numero_permiso;
-        })
-    } catch (err) {
-        console.log(err)
-    }
-}*/
 
-
-var getId = async function (usn) {
+var queryNumPermiso = async function (usn) {
     const pool = await poolPromise
     const fin = pool.request().query("select USN_EST_CODIGO ,USN_ID_USUARIO ,USN_MCL_CODIGO, est_numero_permiso from FP_UsuariosNegocios inner join FP_Estacion fe on usn_est_codigo= fe.EST_CODIGO where USN_ID_USUARIO = '" + usn + "'")
         .then(result => {
             let est_numero_permiso = result.recordset[0].est_numero_permiso;
-            //console.log(est_numero_permiso)
-            //return est_numero_permiso;
-            return est_numero_permiso
+            return est_numero_permiso;
         })
-    return fin
+    return fin;
 };
 
-async function getCompetitors(x, err) {
-    try {
-        const pool2 = poolPromise_everis
-        pool2.request().query("select * from dbo.Competitors where est_numero_permiso ='" + x + "'",
-            function (err, recordset) {
-                if (err) console.log(err)
-                console.log(recordset)
-                return recordset
-            })
-    } catch {
-        console.log(err)
-    }
+var getCompetitors = async function (x) {
+    const pool2 = await poolPromise_everis;
+    const end2 = pool2.request().query("select * from dbo.Competitors where est_numero_permiso ='" + x + "'")
+        .then(result => {
+            console.log(result);
+            return result;
+        })
+    return end2;
 }
 
 module.exports = router
